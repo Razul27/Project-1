7ffaee9941f6b8211a5327e315b2628d91cad207
Razul27 <mohamedrazul916@gmail.com>
2022-07-21 16:01:44 +0530

    New code

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,26 @@
+package org.add;
+
+public class GreensTech {
+	//Datatype Order
+	
+	private void greensOmr(String name,int year) {
+		System.out.println("The Name of the Institution is "+name);
+		System.out.println("The Institute start in the year "+year);
+	}
+	//Datatype
+	private void greensOmr(char logo) {
+		System.out.println("The Institution logo is "+logo);
+	}
+	//Datatype number or count
+		private void greensOmr(int pinCode,String address) {
+			System.out.println("The Institution pincode is "+pinCode);
+			System.out.println("The address of the Institution is "+address);
+		}
+		public static void main(String[] args) {
+			GreensTech g=new GreensTech();
+			g.greensOmr("GreensTech", 2010);
+			g.greensOmr('G');
+			g.greensOmr(600092, "Opposite to Sankeetha hotel, Thoraipakkam");
+		}
+	}
+
